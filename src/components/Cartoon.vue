242f3789c56ab39ccf5d2a268321f77ca6b3fd3e
Luffy-oof <terrence.c520@gmail.com>
2024-07-05 15:21:36 +0800

    Add files via upload

---
+++ src/components/Cartoon.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <div class="row reverse q-mt-xs-md q-mt-md-lg full-width items-center" style="height: 400px; background-color: #FFFAE0">
+      <div class="col-xs-12 col-md-5">
+        <q-video
+          :ratio="16/6"
+          style="max-height: 100%"
+          src="https://www.youtube.com/embed/Kfx-UPBOOZw?rel=0"
+        />
+      </div>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">The Best Cartoon</h4>
+        <h6 class="4HbFGJhOFcE">One Piece is SO FUNNY! Luffy always say something that make me laugh !!</h6>
+      </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Sections.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+  <div class="row">
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E0FFFF">
+      <q-img
+        src="~assets/500.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My Trumpet</h4>
+        <h6 class="q-pa-xl">My Trumpet is the cheapest trumpet in yamaha , but I can play it nicely and noisy(sometimes)</h6>
+      </div>
+    </div>
+
+
+    <div class="row reverse q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #FFE5E0">
+      <q-img
+        src="~assets/400.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My favourite sport</h4>
+        <h6 class="q-pa-xl">swimming  is amazing ! Not only make us cooler but also can sterch our body!!</h6>
+      </div>
+    </div>
+
+
+    <div class="row q-mt-xs-md q-mt-md-lg full-width items-center" style="background-color: #E6E0FF">
+      <q-img
+        src="~assets/300.png"
+        fit="contain"
+        class="col-xs-12 col-md-5"
+      ></q-img>
+      <div class="text-center col-xs-12 col-md-7 q-px-xs-md q-px-md-xl column q-mt-xs-md">
+        <h4 class="text-weight-bolder text-primary q-pb-lg feeling-passionate">My Favourite Activity</h4>
+        <h6 class="q-pa-xl">My favorite activity is playing chess. The strategic depth and complexity of the game captivate me.</h6>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
